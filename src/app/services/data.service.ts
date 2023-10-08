503d4417083c161d7b653425998c6b76cad751b5
Robert Thomas <robertkylethomas@gmail.com>
2023-10-08 20:38:44 +0200

    Added table headers

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map, tap } from 'rxjs';
+import { Observable, delay, map, tap } from 'rxjs';
 import {
   IGoals,
   ILeague,
@@ -17,22 +17,25 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   getStandings(leagueId: number): Observable<any> {
-    return this.http
-      .get<IPayload>(
-        `https://v3.football.api-sports.io/standings?league=${leagueId}&season=2023`
-      )
-      .pipe(map((payload: IPayload) => payload.response[0].league));
+    const prod: string = `https://v3.football.api-sports.io/standings?league=${leagueId}&season=2023`;
+    const dev: string = '../../assets/json/standings.json';
+    return this.http.get<IPayload>(dev).pipe(
+      delay(2300),
+      map((payload: IPayload) => payload.response[0].league)
+    );
   }
 
   getFixtures(
     leagueId: number,
     teamId: number
   ): Observable<IFixtureResponse[]> {
+    const prod: string = `https://v3.football.api-sports.io/fixtures?team=${teamId}&season=2023&league=${leagueId}&last=10`;
+    const dev: string = '../../assets/json/fixtures.json';
     return this.http
-      .get(
-        `https://v3.football.api-sports.io/fixtures?team=${teamId}&season=2023&league=${leagueId}&last=10`
-      )
+      .get(dev)
+
       .pipe(
+        delay(2300),
         map((fixtureResponse: IFixturePayload) => fixtureResponse.response)
       );
   }
